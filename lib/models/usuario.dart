51a08780503849c7acd5ab485cab2441d666b097
brayanbertan <brayanbertan@gmail.com>
2021-03-29 19:13:55 -0300

    usuario model

---
+++ lib/models/usuario.dart
@@ -0,0 +1,28 @@
+class Usuario {
+  String id;
+  String nome;
+  String email;
+  String senha;
+  DateTime dataNascimento;
+
+  Usuario({this.id, this.nome, this.email, this.senha, this.dataNascimento});
+
+  Map<String, dynamic> tojson() {
+    return {
+      'nome': this.nome,
+      'email': this.email,
+      'senha': this.senha,
+      'dataNascimento': this.dataNascimento,
+    };
+  }
+
+  factory Usuario.fromJson(Map json) {
+    return Usuario(
+      id: json['_id'] ?? '',
+      nome: json['nome'] ?? '',
+      email: json['email'] ?? '',
+      senha: json['senha'] ?? '',
+      dataNascimento: json['dataNascimento'] ?? DateTime.now(),
+    );
+  }
+}
